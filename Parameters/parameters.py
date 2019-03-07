72e7432f6378aa6b15f0940f5b5323e4ef90e4cd
mpross512 <mpross512@gmail.com>
2019-03-07 00:13:18 -0600

    More Parameters

--- Parameters/parameters.py
+++ Parameters/parameters.py
@@ -1,4 +1,6 @@
-def testScore(numCorrect, total):
+def testScore(numCorrect, total = 100):
     return numCorrect / total * 100
 
-print(testScore(87, 93))
+print("Test Score:", testScore(87, 93))
+print("Unspecified Test Score:", testScore(87))
+print("Specified Paraneter:", testScore(87, total = 90))
